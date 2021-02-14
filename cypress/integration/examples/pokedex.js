cf5a73bbdf173d2ca9970b58f01113a1cb98d203
yousufali <yousaf.syed5@gmail.com>
2021-02-14 14:20:47 +0500

    e2e test modifing

--- cypress/integration/examples/pokedex.js
+++ cypress/integration/examples/pokedex.js
@@ -1,11 +1,11 @@
 describe('Pokedex', () => {
-    it('front page can be opened', () => {
-        cy.visit('http://localhost:5000')
-        cy.contains('ivysaur')
-        cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
-    })
-    it('Navigating from main page to a particular pokemon'), () => {
-        cy.visit('http://localhost:5000/pokemon/ivysaur')
-        cy.contains('chlorophyll')
-    }
+  it('front page can be opened', () => {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+})
+  it('Navigating from main page to a particular pokemon'), () => {
+    cy.visit('http://localhost:5000/pokemon/ivysaur')
+    cy.contains('chlorophyll')
+}
 })
