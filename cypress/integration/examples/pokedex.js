c4b22d014c28550413773a9d776c263863f3050b
yousufali <yousaf.syed5@gmail.com>
2021-02-14 15:08:28 +0500

    ex11.9 complete

--- cypress/integration/examples/pokedex.js
+++ cypress/integration/examples/pokedex.js
@@ -5,8 +5,8 @@ describe('Pokedex', () => {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
-  it('Navigating from main page to a particular pokemon'), () => {
-    cy.visit('http://localhost:5000/pokemon/ivysaur')
+  it('Navigating from main page to a particular pokemon page', () => {
+    cy.contains('ivysaur').click()
     cy.contains('chlorophyll')
-  }
+  })
 })
